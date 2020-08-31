b1917929ae353da00ea76ac612f2aea3d383698a
sidchiku9 <sidchiku9@gmail.com>
2020-08-31 10:54:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composeone/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.composeone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.composeone.ui.ComposeOneTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Siddharth")
+                }
+            }
+    
+    @Composable
+    fun Greeting(name : String){
+        Text("Hello $name")
+    }
+        }
+
+
