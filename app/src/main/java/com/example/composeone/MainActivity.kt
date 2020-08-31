1e0063bf1fbc92ae8c188e2f4546bbdec61e3908
sidchiku9 <sidchiku9@gmail.com>
2020-08-31 11:41:14 +0530

    Add ColumnModifier

--- app/src/main/java/com/example/composeone/MainActivity.kt
+++ app/src/main/java/com/example/composeone/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.example.composeone.ui.ComposeOneTheme
 
@@ -21,7 +24,8 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun NewsStory(){
-        Column{
+        Column(
+                modifier = Modifier.padding(15.dp) ){
             Text("A day in Shark Fin Cove")
             Text("Raipur, Chhattisgarh")
             Text("August 2020")
